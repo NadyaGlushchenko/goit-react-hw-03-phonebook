0b343dc23e5e70d56c499a6b2b5be0f85129e02e
NadyaGlushchenko <Nadyaglushchenko@ukr.net>
2020-12-16 11:18:14 +0200

    Updates

---
+++ precache-manifest.eb66bdad73af87434ecb23461d140af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475bd726500c5c5079c40f10bab78973",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "2fd36edae1524092d325",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.69f9c056.chunk.css"
+  },
+  {
+    "revision": "fcf66ac1d56c29a7a922",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.3c0e55ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.3c0e55ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fd36edae1524092d325",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.43aa5d88.chunk.js"
+  },
+  {
+    "revision": "8b583280b34de9d01efa",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.8935b325.js"
+  }
+]);
